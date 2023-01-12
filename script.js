8f2e43096456e2319ed90a6b8a59b83567954013
Guntur Santhosh Kumar <107132991+santhosh22G@users.noreply.github.com>
2023-01-12 21:19:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+let input_text = document.getElementById("evaluatedText");
+let charaCount= document.getElementById("letterCount");
+input_text.addEventListener("input",()=>{
+    charaCount.textContent = input_text.value.length;
+});
